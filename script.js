574a92e7ed62de40dbcdd6c05ba83f468699a40f
kamilGit <hantsky123@gmail.com>
2020-04-25 18:20:32 +0200

    Memory Game (Basic)

---
+++ script.js
@@ -0,0 +1,36 @@
+var cards, click, previousCard;
+cards = document.querySelectorAll('.card');
+click = 0;
+previousCard = null;
+
+cards.forEach(card => card.addEventListener('click', flip));
+
+function flip() {
+    click = click + 1;
+    this.classList.toggle('flip');
+    toggleImage(this);
+
+    if(click % 2 == 1) {
+        previousCard = this;
+        return;
+    }
+
+    if (isMatch(previousCard, this)) {
+        return
+    }
+
+    toggleImage(this);
+    toggleImage(previousCard);
+};
+
+function isMatch(firstCard, secondCard) {
+    img1 = firstCard.querySelector('.back').src;
+    img2 = secondCard.querySelector('.back').src;
+
+    return img1 == img2;
+};
+
+function toggleImage(card) {
+    card.querySelector('.front').classList.toggle('hide');
+    card.querySelector('.back').classList.toggle('show');
+};
